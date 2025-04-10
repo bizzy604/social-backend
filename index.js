b7d0a3cf746fa474b159e412fb0d6e68542a4940
Egole <emachartv@gmail.com>
2025-04-10 12:11:03 +0300

    Backend integration

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import { ApolloServer } from "@apollo/server"
 import { expressMiddleware } from "@apollo/server/express4"
+import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer"
 import express from "express"
 import http from "http"
 import cors from "cors"
